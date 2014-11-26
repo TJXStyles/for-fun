ea2bc7507f8d5bf2e76e10314f3625a780128993
Jack Cooc <jack.cooc@gmail.com>
2014-11-26 13:57:46 -0500

    fix var

--- day-2/script.js
+++ day-2/script.js
@@ -1,9 +1,9 @@
 $(function() {
 
 	$('button').on('click', function() {
-		var rgb1 = Math.floor(Math.random() * 255); 
-		var rgb2 = Math.floor(Math.random() * 255);
-		var rgb3 = Math.floor(Math.random() * 255);
+		var rgb1 = Math.floor(Math.random() * 255),
+				rgb2 = Math.floor(Math.random() * 255),
+				rgb3 = Math.floor(Math.random() * 255);
 		$('body').css('background-color','rgb('+rgb1+','+rgb2+','+rgb3+')');
 	});
 });
