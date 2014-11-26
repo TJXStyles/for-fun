515f50615eb446992ae94713ddc69db8f104ecb8
Jack Cooc <jack.cooc@gmail.com>
2014-11-26 14:01:00 -0500

    refactor background generator

--- day-2/script.js
+++ day-2/script.js
@@ -1,9 +1,8 @@
 $(function() {
-
 	$('button').on('click', function() {
-		var rgb1 = Math.floor(Math.random() * 255),
-				rgb2 = Math.floor(Math.random() * 255),
-				rgb3 = Math.floor(Math.random() * 255);
-		$('body').css('background-color','rgb('+rgb1+','+rgb2+','+rgb3+')');
+		var randomColor = function() {
+			return Math.floor(Math.random() * 255);
+		};
+		$('body').css('background-color','rgb('+randomColor()+','+randomColor()+','+randomColor()+')');
 	});
 });
