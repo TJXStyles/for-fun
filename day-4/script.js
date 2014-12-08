263fded82ec6af152208aa377ca5256360b9fde7
Jack Cooc <jack.cooc@gmail.com>
2014-12-08 12:23:19 -0500

    add mouseout event

--- day-4/script.js
+++ day-4/script.js
@@ -1,7 +1,15 @@
-$(function(){
+$(function() {
   // $('dd').addClass('hide');
+  var $dl = $('dl');
   $('dd').hide();
-  $('dl').on('mouseenter', 'dt', function() {
-    $(this).next().slideDown(200).siblings('dd').slideUp(200);
+  $dl.on('mouseenter', 'dt', function() {
+    $(this)
+      .next()
+        .slideDown(200)
+        .siblings('dd')
+          .slideUp(200);
+  });
+  $dl.on('mouseout', 'dt', function() {
+    $(this).siblings('dd').slideUp(200);
   });
 });
