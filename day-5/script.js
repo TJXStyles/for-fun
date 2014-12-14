8aad9ce5cd76cfa87230595294e8e8b4f923552f
Jack Cooc <jack.cooc@gmail.com>
2014-12-14 16:04:00 -0500

    more refactor

--- day-5/script.js
+++ day-5/script.js
@@ -1,7 +1,9 @@
 $(function() {
 	var arr = [],
-			cheat = 'upupdowndownleftrightleftrightbastart',
-			eventButtons = ["left", "up", "down", "right", "select", "start", "b", "a"];
+			btnText,
+			deactivate,
+			cheatMsgRight = $('.cheat-message-right'),
+			cheat = 'upupdowndownleftrightleftrightbastart';
 
 // Resets the cheat code if it's too long
 	function codeReset(array) {
@@ -15,174 +17,34 @@ $(function() {
 		var newArray = array.join(''),
 				codeArea = $('.cheat-code-area');
 		if (newArray == cheat) {
-			codeArea.hide();
-			$('.cheat-message-right').show();
+			cheatMsgRight.show();
 			setInterval(blink, 500);
 		} else {
-			codeArea.hide();
 			$('.cheat-message-wrong').show();
 		}
 	}
 
 	function blink() {
-		var cheatMsg = $('.cheat-message-right');
-		cheatMsg.fadeIn(200);
-		cheatMsg.fadeOut(200);
+		cheatMsgRight.fadeIn(200).fadeOut(200);
 	}
 
 	$('.reload').on('click', function() {
 		location.reload();
 	});
 
-	$('.up, .down, .left, .right').on('mousedown', function() {
-		var btnText = $(this).data('button');
+	$('.up, .down, .left, .right, .select, .start, .b, .a').on('mousedown', function() {
+		btnText = $(this).data('button');
 		$(this).addClass(btnText+'-activate');
 		arr.push(btnText);
+		$('.cheat-code-area').text(arr.join(' '));
 		if (arr.length === 11) {
 			codeConfirmed(arr);
 		}
 	}).on('mouseup', function() {
-		var btnText = $(this).data('button');
-		$(this).removeClass( btnText+'-activate');
+		remove = $(this).removeClass(btnText+'-activate');
+		deactivate;
 	}).on('mouseleave', function() {
-		var btnText = $(this).data('button');
-		$(this).removeClass($(this)+'-activate');
+		deactivate;
 	});
 
-/*
-***** Button event handlers begin *****
-*/
-//D-Pad UP
-	// $('.up').on('mousedown', function() {
-	// 	$(this).addClass('up-activate');
-	// 	var btnText = $(this).data('button');
-	// 	arr.push(btnText);
-	// 	$('.cheat-code-area').text(arr.join(' '));
-	// 	if (arr.length === 11) {
-	// 		codeConfirmed(arr);
-	// 	}
-	// }).on('mouseup', function() {
-	// 	$(this).removeClass('up-activate');
-	// }).on('mouseleave', function() {
-	// 	$(this).removeClass('up-activate');
-	// });
-
-// D-Pad RIGHT
-// 	$('.right').on('mousedown', function() {
-// 		$(this).addClass('right-activate');
-// 		var btnText = $(this).data('button');
-// 		arr.push(btnText);
-// 		$('.cheat-code-area').text(arr.join(' '));
-// 		if (arr.length === 11) {
-// 			codeConfirmed(arr);
-// 		}
-// 	}).on('mouseup', function() {
-// 		$(this).removeClass('right-activate');
-// 	}).on('mouseleave', function() {
-// 		$(this).removeClass('right-activate');
-// 	});
-
-// // D-Pad DOWN
-// 	$('.down').on('mousedown', function() {
-// 		$(this).addClass('down-activate');
-// 		var btnText = $(this).data('button');
-// 		arr.push(btnText);
-// 		$('.cheat-code-area').text(arr.join(' '));
-// 		if (arr.length === 11) {
-// 			codeConfirmed(arr);
-// 		}
-// 	}).on('mouseup', function() {
-// 		$(this).removeClass('down-activate');
-// 	}).on('mouseleave', function() {
-// 		$(this).removeClass('down-activate');
-// 	});
-
-// //D-Pad LEFT
-// 	$('.left').on('mousedown', function() {
-// 		$(this).addClass('left-activate');
-// 		var btnText = $(this).data('button');
-// 		arr.push(btnText);
-// 		$('.cheat-code-area').text(arr.join(' '));
-// 		if (arr.length === 11) {
-// 			codeConfirmed(arr);
-// 		}
-// 	}).on('mouseup', function() {
-// 		$(this).removeClass('left-activate');
-// 	}).on('mouseleave', function() {
-// 		$(this).removeClass('left-activate');
-// 	});
-
-// //SELECT/START
-// 	$('.select, .start').on('mousedown', function() {
-// 		$(this).addClass('ss-activate');
-// 		var btnText = $(this).data('button');
-// 		arr.push(btnText);
-// 		$('.cheat-code-area').text(arr.join(' '));
-// 		if (arr.length === 11) {
-// 			codeConfirmed(arr);
-// 		}
-// 	}).on('mouseup', function() {
-// 		$(this).removeClass('ss-activate');
-// 	}).on('mouseleave', function() {
-// 		$(this).removeClass('ss-activate');
-// 	});
-
-// //B-A Buttons
-// 	$('.a, .b').on('mousedown', function() {
-// 		$(this).addClass('button-activate');
-// 		var btnText = $(this).data('button');
-// 		arr.push(btnText);
-// 		$('.cheat-code-area').text(arr.join(' '));
-// 		if (arr.length === 11) {
-// 			codeConfirmed(arr);
-// 		}
-// 	}).on('mouseup', function() {
-// 		$(this).removeClass('button-activate');
-// 	}).on('mouseleave', function() {
-// 		$(this).removeClass('button-activate');
-// 	});
-
 });
-
-// var direction = {
-// 	up: {
-// 		action: "up",
-// 		button: "$('.up')",
-// 		activateClass: "d-pad-up-activate"
-// 	},
-// 	down: {
-// 		action: "down",
-// 		button: "$('.down')",
-// 		activateClass: "d-pad-down-activate"
-// 	},
-// 	left: {
-// 		action: "left",
-// 		button: "$('.left')",
-// 		activateClass: "d-pad-left-activate"
-// 	},
-// 	right: {
-// 		action: "right",
-// 		button: "$('.right')",
-// 		activateClass: "d-pad-right-activate"
-// 	},
-// 	select: {
-// 		action: "select",
-// 		button: "$('.select')",
-// 		activateClass: "ss-activate"
-// },
-// 	start: {
-// 		action: "start",
-// 		button: "$('.start')",
-// 		activateClass: "ss-activate"
-// },
-// 	b: {
-// 		action: "b",
-// 		button: "$('.b')",
-// 		activateClass: "button-pressed"
-// 	},
-// 		a: {
-// 		action: "a",
-// 		button: "$('.a')",
-// 		activateClass: "button-pressed"
-// 	}
-// }
